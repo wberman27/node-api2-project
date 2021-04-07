c5cec9ccaf96f489b01c947fb76d7cd65945f9b3
Will B <wberman27@gmail.com>
2021-04-06 20:18:26 -0400

    adding to post-router

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,61 @@
 // implement your posts router here
+const Posts = require('./posts-model');
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) =>{
+    Posts.find()
+    .then(post =>{
+        res.status(200).json(post)
+    })
+    .catch(err =>{
+        res.status(500).json(err.message)
+    })
+})
+
+router.get('/:id', (req, res) =>{
+    const {id} = req.params
+    Posts.findById(id)
+    .then(post =>{
+        res.status(200).json(post)
+    })
+    .catch(err =>{
+        res.status(500).json(err.message)
+    })
+})
+
+router.post('/', (req, res) =>{
+    const newPost = req.body
+    Posts.insert(newPost)
+    .then(post =>{
+        res.status(201).json(post)
+    })
+    .catch(err =>{
+        res.status(500).json(err.message)
+    })
+})
+
+router.put('/:id', (req, res) =>{
+    const {id} = req.params
+    const updatedPost = req.body
+    Posts.update(id, updatedPost)
+    .then(post =>{
+        res.status(201).json(post)
+    })
+    .catch(err =>{
+        res.status(500).json(err.message)
+    })
+})
+
+router.delete('/:id', (req, res) =>{
+    const {id} = req.params
+    Posts.remove(id)
+    .then(post =>{
+        res.status(201).json(post)
+    })
+    .catch(err =>{
+        res.status(500).json(err.message)
+    })
+})
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,11 +1,11 @@
 // implement your server here
 // require your posts router and connect it here
-const express = require('express')
-const server = express()
-server.use(express.json())
+const express = require('express');
+const postsRouter = require('./posts/posts-router');
+const server = express();
 
-const postsRouter = require('./posts/posts-router')
-server.use('/api/posts', postsRouter)
+server.use(express.json());
+server.use('/api/posts', postsRouter);
 
 server.get('/', (req, res) =>{
     res.send(`
